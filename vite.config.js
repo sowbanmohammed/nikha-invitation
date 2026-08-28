4307affe9d1649cd725d4ac574159139c3bfe666
sowbanmohammed <mohammedsowban0044@gmail.com>
2026-08-28 17:27:04 +0530

    Remove base path from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/nikha-invitation/",
   plugins: [react(), tailwindcss()],
 });
