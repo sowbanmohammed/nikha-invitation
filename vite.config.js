f31cb55f4d9ec8df15f5c61a2a1c452e8c87d8b1
sowbanmohammed <mohammedsowban0044@gmail.com>
2026-08-28 16:44:30 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/nikha-invitation/",
   plugins: [react(), tailwindcss()],
 });
